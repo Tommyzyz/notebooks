7ed5905320fe61ec728eb224060096a9738eec58
Tommyzyz <tommyzhao2017@outlook.com>
2024-11-01 16:38:23 +0800

    提交Plot.ipynb 示例代码及其相关图像，新建 VSCode 的配置文件（launch.json 和 tasks.json），并添加 C 语言示例代码 (main.c) 以及其生成的可执行文件 (main.exe

---
+++ C/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int result = 0;
+int main() {
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", i);
+    }
+    scanf("%d", &result);
+    
+    printf("Hello, world!1\n");
+    printf("111111111111\n");
+    printf("Hello, world!2\n");
+    return 0;
+}
